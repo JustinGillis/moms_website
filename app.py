ef90dc9a845b8e22cd92aa151fdaec773bd7618e
Justin.gillis <justin_gillis@outlook.com>
2022-05-03 17:42:36 -0400

    setup

--- app.py
+++ app.py
@@ -1,26 +1,90 @@
-from flask import Flask, redirect
+
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return '<p>redirect to home page</p>'
+@app.route('/')
+def landing():
+    return render_template('landing.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/appointment')
+def about():
+    return render_template('appointment.html')
+
+@app.route('/contact')
+def about():
+    return render_template('contact.html')
 
 
-@app.route("/home")
-def home():
-    return """<p>home page</p>
-              <p></p>"""
 
 
+"""
+from flask import request
 
-@app.route("/about")
-def home():
-    return "<p>about page</p>"
+@app.route('/')
+def index():
+    username = request.cookies.get('username')
+    # use cookies.get(key) instead of cookies[key] to not get a
+    # KeyError if the cookie is missing.
+"""
 
+"""
+from flask import make_response
 
-@app.route("/appointment")
-def home():
-    return "<p>make an appointment page - add online scheduling</p>"
+@app.route('/')
+def index():
+    resp = make_response(render_template(...))
+    resp.set_cookie('username', 'the username')
+    return resp
+"""
 
+"""
+from flask import render_template
 
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('page_not_found.html'), 404
+"""
+
+"""
+from flask import session
+
+# Set the secret key to some random bytes. Keep this really secret!
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+@app.route('/')
+def index():
+    if 'username' in session:
+        return f'Logged in as {session["username"]}'
+    return 'You are not logged in'
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect(url_for('index'))
+    return '''
+        <form method="post">
+            <p><input type=text name=username>
+            <p><input type=submit value=Login>
+        </form>
+    '''
+
+@app.route('/logout')
+def logout():
+    # remove the username from the session if it's there
+    session.pop('username', None)
+    return redirect(url_for('index'))
+"""
+
+"""
+python -c 'import secrets; print(secrets.token_hex())'
+"""
+
+"""
+
+"""
